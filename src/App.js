2ef62fe765a3f135a9f4c134f79cb2ca30d634cd
Shreyanshu Maske <36789736+shreyanshu1966@users.noreply.github.com>
2025-01-03 00:16:20 +0530

    Bootstrap React app with CRA scaffold

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FeedbackSystem from './components/FeedbackSystem';
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <FeedbackSystem />
+    </div>
+  );
+}
+
+export default App;
